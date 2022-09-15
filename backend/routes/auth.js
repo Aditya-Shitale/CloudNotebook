61a538edf544f903f6be10bf01c9e0ede25658fc
Aditya <adi454282@gmail.com>
2022-09-15 23:51:28 +0530

    day 2.2 thunderclient collection with try-catch fuction

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,29 +1,45 @@
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const User=require('../models/User');
-const router=express.Router();
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+const router = express.Router();
 
-
-
-//create a user using :POST "/api/auth/" dosent require auth
-router.post('/',[
-    body('name','Enter valid name').isLength({ min: 3 }),
-    body('email','Enter valid email').isEmail(),
-    body('password').isLength({ min: 5 })
-],(req,res)=>{
-    
+//create a user using :POST "/api/auth/createUser" No login required dosent require auth
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter valid name").isLength({ min: 3 }),
+    body("email", "Enter valid email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //if there are errors return bad request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    //check whether email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorryy..!!A user Already exist with this email id" });
+      }
+      user = User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err=>{console.log(err) 
-        res.json({error:'please enter a unique email',message: err.message}) })
-})
+      });
+      // .then(user => res.json(user))
+      // .catch(err=>{console.log(err)
+      //   res.json({error:'please enter a unique email',message: err.message}) })
+      res.json(req.body);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("error 404");
+    }
+  }
+);
 
-module.exports = router
-
+module.exports = router;
