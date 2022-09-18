608b9879ff9c97942d020c42acf0a9dd0b1b8fea
Aditya <adi454282@gmail.com>
2022-09-18 19:41:08 +0530

    day 3.1 update notes added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fetchuser = require("../middleware/fetchuser");
 const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator");
+const { findById, findByIdAndUpdate } = require("../models/Note");
 
 //Router 1:  get all note using using :GET "/api/auth/createUser" No login required dosent require auth
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
@@ -15,7 +16,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
-//Router 2:  Add a new note using using :POST "/api/auth/addnote" login required
+//Router 2:  Add a new note using using :POST "/api/notes/addnote" login required
 router.post(
   "/addnote",
   fetchuser,
@@ -48,5 +49,29 @@ router.post(
     }
   }
 );
+
+
+//Router 3:  Update a note using :POST "/api/notes/addnote" login required
+router.put( "/updatenote/:id",fetchuser,  async (req, res) => {
+
+        const {title,description,tag}=req.body;
+        //create newNote object
+        const newNote={};
+        if(title){newNote.title=title};
+        if(description){newNote.description=description};
+        if(tag){newNote.tag=tag};
+
+        //Find the note to be updated and update it
+        let note =await Note.findById(req.params.id);
+        if(!note){ return res.status(404).send("Not found")}
+        
+        //note.user.toString gives id of this note
+        if(note.user.toString()!==req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+
+        note =await Note.findByIdAndUpdate(req.params.id,{$set: newNote},{new:true})
+        res.json({note});
+    })
 
 module.exports = router;
