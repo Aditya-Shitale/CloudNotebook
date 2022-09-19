7a703e380f2a317b3fadc145552c84a96774e5a6
Aditya <adi454282@gmail.com>
2022-09-19 22:19:48 +0530

    day 5.1 uselocation hook used ,Bootstrap added

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,21 +3,10 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState =(props)=>{
-        const s1 ={
-            "name":"adi",
-            "class": "12b"
-        }
-        const [state, setState]= useState(s1);
-       const update=()=>{
-            setTimeout(() => {
-                setState({
-                    "name":"aditya",
-                    "class": "11c"
-                })
-            }, 1000);
-        }
+
+ 
         return(
-            <NoteContext.Provider value={{state:state,update:update}}>
+            <NoteContext.Provider value={{}}>
                 {props.children}
             </NoteContext.Provider>
         )
